0c9785f7e00216608c3e5a7c41f0779930917058
gargi_mahadeshwar <gjm21@duke.edu>
2023-10-02 21:43:39 -0400

    adding database support for wishlist

--- mini-amazon-skeleton-main/db/load.sql
+++ mini-amazon-skeleton-main/db/load.sql
@@ -14,3 +14,8 @@ SELECT pg_catalog.setval('public.products_id_seq',
 SELECT pg_catalog.setval('public.purchases_id_seq',
                          (SELECT MAX(id)+1 FROM Purchases),
                          false);
+
+\COPY Wishes FROM 'Wishes.csv' WITH DELIMITER ',' NULL '' CSV
+SELECT pg_catalog.setval('public.wishes_id_seq',
+                         (SELECT MAX(id)+1 FROM Wishes),
+                         false);
