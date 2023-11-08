5d10f8a83387d6a3e5d0c9bcd871277c8b0a2c89
pradneshkolluru <pradnesh.kolluru@duke.edu>
2023-11-07 23:41:47 -0500

    added pagination for products - max 10 per page

--- mini-amazon-skeleton-main/db/load.sql
+++ mini-amazon-skeleton-main/db/load.sql
@@ -5,7 +5,7 @@ SELECT pg_catalog.setval('public.users_id_seq',
                          (SELECT MAX(id)+1 FROM Users),
                          false);
 
-\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
+\COPY Products FROM 'Products copy.csv' WITH DELIMITER ',' NULL '' CSV
 SELECT pg_catalog.setval('public.products_id_seq',
                          (SELECT MAX(id)+1 FROM Products),
                          false);
