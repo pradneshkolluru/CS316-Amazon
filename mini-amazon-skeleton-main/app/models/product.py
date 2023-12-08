fb80c63218f628c612af6eb7190a7842bbac42dc
pradneshkolluru <pradnesh.kolluru@duke.edu>
2023-12-08 15:01:31 -0500

    adding custom images for each product

--- mini-amazon-skeleton-main/app/models/product.py
+++ mini-amazon-skeleton-main/app/models/product.py
@@ -1,4 +1,45 @@
 from flask import current_app as app
+import io
+import base64
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import numpy as np
+matplotlib.use('Agg')
+
+
+def adjust_brightness(img, factor):
+    """Adjust the brightness of the image."""
+    hsv = mcolors.rgb_to_hsv(img[:, :, :3])  # Convert RGB to HSV
+    hsv[:, :, 2] *= factor  # Adjust the V channel (brightness)
+    return mcolors.hsv_to_rgb(hsv)  # Convert back to RGB
+
+
+def imagePic(name, pid):
+
+    fig, ax = plt.subplots(figsize=(4, 4))
+
+    bg_image = plt.imread('app/static/images/gargi.jpeg')
+
+    bg_image = adjust_brightness(bg_image, 0.95)
+
+    ax.imshow(bg_image, extent=[0, 1, 0, 1], zorder=-1)
+
+    # background_color = np.random.rand(3,)  # Generating a random RGB color
+    # plt.figure(figsize=(4, 4), facecolor=background_color)  # Set figure size and background color
+
+    plt.text(0.5, 0.8, str(pid), fontsize=20, ha='center', color='black')
+    plt.text(0.5, 0.9, str(name), fontsize=10, ha='center', color='black')
+
+    plt.axis('off')
+
+    buff = io.BytesIO()
+    plt.savefig(buff, format='png')
+    buff.seek(0)
+    encodedPlot = base64.b64encode(buff.read()).decode('utf-8')
+
+
+    return encodedPlot
 
 
 class Product:
@@ -10,6 +51,7 @@ class Product:
         self.category = category
         self.available = available
         self.avgRating = avgRating
+        self.image = imagePic(self.name, self.id)
 
     @staticmethod
     def get(id):
@@ -53,6 +95,7 @@ class Product:
         print(query)
 
         rows = app.db.execute(query, **params)
+
         return [Product(*row) for row in rows]
 
     
@@ -91,6 +134,8 @@ class Product:
         print(query)
 
         rows = app.db.execute(query, **params)
+
+        print(rows)
         return [Product(*row) for row in rows]
 
 
