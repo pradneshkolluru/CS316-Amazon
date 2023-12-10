4c3fa52e18fb54073dd9920546994a05133d6dd8
Lindsay <rll46@duke.edu>
2023-12-10 12:32:27 -0500

    got rid of print statement

--- mini-amazon-skeleton-main/app/users.py
+++ mini-amazon-skeleton-main/app/users.py
@@ -166,8 +166,6 @@ def public_view(id):
         reviews = None
         reviewMetrics = None
     
-    for i in reviews:
-        print(i.first_name)
     return render_template('public_user.html', user_info=user_info, seller=seller, review_history=reviews, avail_products=sliced_products, reviewMetrics = reviewMetrics, pagination=pagination)
 
 @bp.route('/logout')
